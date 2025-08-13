ac546c1f10d75dae01a28cbdaf8870705ecf364b
Moe <malash.mohamed.1@gmail.com>
2025-08-13 00:44:18 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
-// /lib/firebase.js
-import { initializeApp, getApps } from "firebase/app";
+// firebase.js
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -8,8 +8,8 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
