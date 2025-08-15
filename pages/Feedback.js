5010c84796440afde189a69f75652905e779b164
Moe <malash.mohamed.1@gmail.com>
2025-08-15 03:20:18 -0400

    Update Feedback.js

--- pages/Feedback.js
+++ pages/Feedback.js
@@ -1,20 +1,7 @@
-// Feedback.js
+// pages/feedback.js
 import { useState } from 'react';
-import { getFirestore, collection, addDoc, serverTimestamp } from 'firebase/firestore';
-import { initializeApp } from 'firebase/app';
-
-// Your Firebase config (from firebase.js)
-const firebaseConfig = {
-  apiKey: AIzaSyAG-iJcrjxtwDP-u65kM7MOKUvabE4_KJI,
-  authDomain: life-app-752db.firebaseapp.com,
-  projectId: life-app-752db,
-  storageBucket: life-app-752db.firebasestorage.app,
-  messagingSenderId: 532680973641,
-  appId: 1:532680973641:web:e27b88f43175894d808e98,
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { db } from '../lib/firebase'; // <- using the new firebase.js
 
 export default function Feedback() {
   const [feedback, setFeedback] = useState('');
